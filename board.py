2583398c0501636636082bf3a388275d36ddfd2a
jhnkng <96804912+jhnkng@users.noreply.github.com>
2022-02-20 15:49:17 +0900

    Refactored creating card/list objects index to its own function

--- board.py
+++ board.py
@@ -1,7 +1,6 @@
 
 class Board:
     def __init__(self):
-        # self.new_id = shinrin.get_data('id')
         self.user_key = 'new_user'
         self.current_board_index = {}
         self.current_list_objects = []
@@ -9,11 +8,8 @@ class Board:
         self.current_card_objects = []
         self.current_card_obj_index = {}
         self.current_card_id = str
-        # self.current_card_content = str
         self.current_list_id = str
         self.current_list_name = str
-        # self.current_notebook_id = int
-        # self.current_notebook_name = str
 
     def create_list_objects(self, saved_list_data):
         list_objects = []
@@ -21,7 +17,6 @@ class Board:
             new_list_obj = List()
             new_list_obj.list_id = each_list['list_id']
             new_list_obj.list_name = each_list['list_name']
-            # new_list_obj.is_notebook = each_list['is_notebook']
             new_list_obj.cards = each_list['cards']
             list_objects.append(new_list_obj)
         self.current_list_objects = list_objects
@@ -37,6 +32,16 @@ class Board:
             card_objects.append(new_card_obj)
         self.current_card_objects = card_objects
         return self.current_card_objects
+
+    def create_list_objects_index(self):
+        self.current_list_obj_index = {each.list_id: self.current_list_objects.index(each)
+                                       for each in self.current_list_objects}
+        print(f"List Obj Index {self.current_list_obj_index}")
+
+    def create_card_objects_index(self):
+        self.current_card_obj_index = {each.card_id: self.current_card_objects.index(each)
+                                       for each in self.current_card_objects}
+        print(f"Card Obj Index {self.current_card_obj_index}")
 
 
 class List:
